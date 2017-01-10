79f59b93a84ff829c1e73d05a9c17c4ee4b4b540
sinsoku <sinsoku.listy@gmail.com>
2017-01-10 20:55:06 +0900

    :pill: Fix an error that container does not exist

--- bin/ci.sh
+++ bin/ci.sh
@@ -2,7 +2,7 @@
 set -ev
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-  packer build -only=amazon-ebs template.json
+  packer build template.json
 else
   packer build -only=docker template.json
 fi
