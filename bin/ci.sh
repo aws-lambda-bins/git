038721ab6e9e1ada0965f2831cad778020279110
sinsoku <sinsoku.listy@gmail.com>
2017-01-14 11:27:47 +0900

    :sparkles: Save an archive file with branch or tag

--- bin/ci.sh
+++ bin/ci.sh
@@ -3,6 +3,10 @@ set -ev
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
   packer build template.json
+  mv dest/git-* dest/git-master.tar.gz
+elif [ -n "${TRAVIS_TAG}" ]; then
+  packer build template.json
+  mv dest/git-* dest/git-${TRAVIS_TAG}.tar.gz
 else
   packer build -only=docker template.json
 fi
