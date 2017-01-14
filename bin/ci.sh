3d57e8284146c390df1a22d43e98f2a96d60e0e1
sinsoku <sinsoku.listy@gmail.com>
2017-01-14 15:36:33 +0900

    :recycle: Simplify the condition

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -ev
 
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
+if [[ "${TRAVIS_BRANCH}" = "master" || -n "${TRAVIS_TAG}" ]]; then
   packer build template.json
-  mv dest/git-* dest/git-master.tar.gz
-elif [ -n "${TRAVIS_TAG}" ]; then
-  packer build template.json
-  mv dest/git-* dest/git-${TRAVIS_TAG}.tar.gz
 else
   packer build -only=docker template.json
 fi
